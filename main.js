6c58e01fc2ab3cb891ed7a425063a91aff037d64
bilalbuttnazbutt <164246858+bilalbuttnazbutt@users.noreply.github.com>
2024-04-25 17:33:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var froutes = ["apple", "banana", "mango", "grapes"];
+for (var i = 0; i < froutes.length; i++) {
+    console.log(froutes[i]);
+}
---
+++ main.ts
@@ -0,0 +1,4 @@
+let froutes: string[] = [`apple`,`banana`,`mango`,`grapes`];
+for(let i=0; i<froutes.length; i++){
+    console.log(froutes[i]);
+}
